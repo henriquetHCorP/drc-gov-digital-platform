fd8a07eec2caf2726b938b5803ddd8446e5ef97a
henriquet kapema <henriquetkapema@gmail.com>
2024-10-13 21:27:59 +0100

    Create pages and routes

---
+++ src/app/(others)/users/[username]/page.jsx
@@ -0,0 +1,10 @@
+// in the filename, [] means that information in between is dynamic and () means the file will be ignored by the system, this is just meant for files organization; 
+import React from 'react'
+
+export default function UserPage() {
+  return (
+    <div>
+      UserPage
+    </div>
+  )
+}
