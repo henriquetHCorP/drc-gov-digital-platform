59828d72f9d2dce9a1bec18d4b0510105a022b0e
henriquet kapema <henriquetkapema@gmail.com>
2024-10-15 19:33:59 +0100

    Add authentication using clerk

--- src/app/(others)/layout.jsx
+++ src/app/(others)/layout.jsx
@@ -1,7 +1,11 @@
 import localFont from "next/font/local";
 import ".././globals.css";
-import LeftSidebar from "@/components/LeftSidebar";
-import RightSidebar from "@/components/RightSidebar";
+
+import { ClerkProvider, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+
+import LeftSidebar from "../../components/LeftSidebar";
+import RightSidebar from "../../components/RightSidebar";
+import Loader from "../../components/Loader";
 
 const geistSans = localFont({
   src: ".././fonts/GeistVF.woff",
@@ -21,22 +25,30 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ClerkLoading>
+          <Loader />
+        </ClerkLoading>
+        <ClerkLoaded>
         <div className="flex justify-between max-w-6xl mx-auto">
           <div className="hidden sm:inline border-r h-screen sticky top-0">
             <LeftSidebar />
           </div>
-        <div className="w-2xl flex-1">{children}</div>
-        <div className=" lg:flex-col p-3 h-screen border-l hidden lg:flex w-[24rem]">
+            <div className="w-2xl flex-1">{children}</div>
+            <div className=" lg:flex-col p-3 h-screen border-l hidden lg:flex w-[24rem]">
+           
          <RightSidebar /> 
          </div>
         </div>
         {/* div with no leftsidebar or rightsidebar for the login page */}
+        </ClerkLoaded>
       </body>
 
     </html>
+    </ClerkProvider>
   );
 }
--- src/components/LeftSidebar.jsx
+++ src/components/LeftSidebar.jsx
@@ -1,14 +1,14 @@
 import Link from 'next/link';
 import { FaXTwitter } from 'react-icons/fa6';
 import { HiHome } from 'react-icons/hi';
-// import {
-//   SignedIn,
-//   SignedOut,
-//   SignInButton,
-//   SignOutButton,
-// } from '@clerk/nextjs';
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
+} from '@clerk/nextjs';
 // import MiniProfile from './MiniProfile';
-export default function Sidebar() {
+export default function LeftSidebar() {
   return (
     <div className='flex flex-col p-3 justify-between h-screen items-center'>
       <div className='flex flex-col gap-4 p-3'>
@@ -22,14 +22,14 @@ export default function Sidebar() {
           <HiHome className='w-7 h-7' />
           <span className='font-bold hidden xl:inline'>Home</span>
         </Link>
-        {/* <button className='bg-blue-400 text-white rounded-full  hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline font-semibold'>
+        <button className='bg-blue-400 text-white rounded-full  hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline font-semibold'>
           <SignedIn>
-            <SignOutButton />
+            <SignOutButton /> 
           </SignedIn>
           <SignedOut>
-            <SignInButton />
+            <SignInButton /> 
           </SignedOut>
-        </button> */}
+        </button>
       </div>
       {/* <SignedIn>
         <MiniProfile />
