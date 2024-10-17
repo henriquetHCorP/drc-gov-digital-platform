d69b2857b6559cd4153858a2a95a9f4d369d10fc
henriquet kapema <henriquetkapema@gmail.com>
2024-10-17 16:38:28 +0100

    webhooks2

--- src/components/LeftSidebar.jsx
+++ src/components/LeftSidebar.jsx
@@ -20,7 +20,7 @@ export default function LeftSidebar() {
           className='flex items-center p-3 hover:bg-gray-100 rounded-full transition-all duration-200 gap-2 w-fit'
         >
           <HiHome className='w-7 h-7' />
-          <span className='font-bold hidden xl:inline'>Home</span>
+          <span className='font-bold hidden xl:inline'>Home-</span>
         </Link>
         <button className='bg-blue-400 text-white rounded-full  hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline font-semibold'>
           <SignedIn>
