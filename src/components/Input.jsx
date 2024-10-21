d75aebf6501849373b43001fb243e7dae7bf76f6
henriquet kapema <henriquetkapema@gmail.com>
2024-10-21 21:16:32 +0100

    Add Firebase storage and complete file upload functionality2

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -108,7 +108,7 @@ export default function Input() {
              disabled
              className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50"
             >
-              Post 
+              Post. 
               </button>  
 
            </div>
