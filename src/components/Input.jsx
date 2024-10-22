50a8f1ff3b618fcc7736bcb5b244f856ab8a87a1
henriquet kapema <henriquetkapema@gmail.com>
2024-10-23 00:21:56 +0100

    Complete upload post functionality

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -12,12 +12,18 @@ import {
     } 
     from 'firebase/storage'; 
 
+
 export default function Input() {
   const { user, isSignedIn, isLoaded } = useUser();
+  console.log("user:", user); 
   const [imageFileUrl, setImageFileUrl] = useState(null); 
   const [selectedFile, setSelectedFile] = useState(null); 
   const [imageFileUploading, setImageFileUploading] = useState(false); 
+  const [text, setText] = useState(''); 
+  const [postLoading, setPostLoading] = useState(false); 
+  
   const imagePickRef = useRef(null); 
+  
 
    
    const addImageToPost = async (e) => {
@@ -63,9 +69,33 @@ export default function Input() {
         }
       ) 
    }
+   const handleSubmit = async() => {
+    setPostLoading(true); 
+    const response = await fetch('api/post/create', {
+      method: 'POST', 
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        userMongoId: user.publicMetadata.userMongoId,
+        name:user.fullName, 
+        username: user.username,
+        text, 
+        profileImg: user.imageUrl, 
+        image:imageFileUrl, 
+        
+      })
+    })
+    setPostLoading(false); 
+    setText('');
+    setSelectedFile(null);
+    setImageFileUrl(null); 
+    // location.reload(); 
+
+   }
    if (!isSignedIn || !isLoaded) { 
-    return null; 
-   } 
+     return null; 
+    } 
   return (
     <div className="flex border-b border-gray-200 p-3 space-x-3 w-full">
       <img 
@@ -78,6 +108,8 @@ export default function Input() {
            className="w-full border-none outline-none tracking-wide min-h-[50px] text-gray-700"
            placeholder="What's happening"
            rows="2"
+           value={text}
+           onChange={(e) => setText(e.target.value)}
            ></textarea>
            {selectedFile && (
             <img 
@@ -106,8 +138,9 @@ export default function Input() {
                 onChange={addImageToPost}
               /> 
             <button
-             disabled
+             disabled={text.trim() ==="" || postLoading || imageFileUploading}
              className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50"
+              onClick={handleSubmit}
             >
               Post
               </button>  
