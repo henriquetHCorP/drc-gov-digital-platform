2948161e4d4646fb2096603cac11c5bd4bcf8fa5
henriquet kapema <henriquetkapema@gmail.com>
2024-10-29 15:39:42 +0100

    Complete post page 2

--- src/app/(others)/posts/[id]/page.jsx
+++ src/app/(others)/posts/[id]/page.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
 
-export default function PostPage() {
+import Link from 'next/link';
+import React from 'react'
+import Post from '../../../../components/Post';
+import Comments from '../../../../components/Comments';
+import { HiArrowLeft } from 'react-icons/hi'; 
+
+export default async function PostPage({ params }) {
+  // we've made this function async because we wanna make it server side
+  let data = null; 
+  try {
+     const result = await fetch(process.env.URL + '/api/post/get', {
+      method: 'POST', 
+      // headers: {
+      //   'Content-Type': 'application/json', 
+      // },
+      body:JSON.stringify({ postId: params.id }), 
+      cache: 'no-store', 
+     });
+     data = await result.json(); 
+    } catch (error) {
+      console.log('Error getting post:', error); 
+      data = { text: 'Failed to load post' }; 
+    }
+    // console.log('result:', result);
   return (
-    <div>
-      PostPage
+    <div className="max-w-xl max-auto border-r border-l min-h-screen">
+      <div className="flex items-center space-x-2 py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
+        <Link href={'/'} className="hover:bg-gray-100 rounded-full p-2">
+         <HiArrowLeft className="h-5 w-5" />
+        </Link>
+        <h2 className="sm:text-lg">Back</h2>
+      </div>
+      {!data && <h2 className="text-center mt-5 text-lg">Post not found</h2>}
+      {data && <Post post={data} />}
+      {data && <Comments comments={data.comments}/> }
     </div>
   )
 }
