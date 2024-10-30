83d9e4e8f471f4caa41d12ec6bf2670a08cddd40
henriquet kapema <henriquetkapema@gmail.com>
2024-10-30 01:18:57 +0100

    test

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,113 +1,104 @@
- 'use client';
-
-import {
-  HiOutlineChat,
-  HiOutlineHeart,
-  HiOutlineTrash,
-  HiHeart,
-} from 'react-icons/hi';
-import { useState, useEffect } from 'react';
-import { useUser } from '@clerk/nextjs';
+"use client"; 
+import { useEffect, useState } from 'react';
+import { HiOutlineChat, HiOutlineHeart, HiOutlineTrash, HiHeart } from 'react-icons/hi';
+import { useUser } from '@clerk/nextjs'; 
 import { useRouter } from 'next/navigation';
-import { modalAtom, postIdAtom } from '@/atom/modalAtom';
-import { useRecoilState } from 'recoil';
-export default function Icons({ post }) {
-  const [isLiked, setIsLiked] = useState(false);
-  const [likes, setLikes] = useState(post.likes || []);
-  const [open, setOpen] = useRecoilState(modalAtom);
-  const [postId, setPostId] = useRecoilState(postIdAtom);
-  const { user } = useUser();
-  const router = useRouter();
 
-  const likePost = () => {
-    if (!user) {
-      return router.push('/sign-in');
-    }
-    const like = fetch('/api/post/like', {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ postId: post._id }),
-    });
-    if (like && isLiked) {
-      setLikes(
-        likes.filter((like) => like !== user.publicMetadata.userMongoId)
-      );
-    }
-    if (like && !isLiked) {
-      setLikes([...likes, user.publicMetadata.userMongoId]);
-    }
-  };
-  useEffect(() => {
-    if (user && likes?.includes(user.publicMetadata.userMongoId)) {
-      setIsLiked(true);
-    } else {
-      setIsLiked(false);
-    }
-  }, [likes, user]);
+import { modalAtom, postIdAtom } from '../atom/modalAtom'; 
+import { useRecoilState } from 'recoil';   
+export default function Icons({post}) {
+    const [isLiked, setIsLiked] = useState(false); 
+    const [likes, setLikes] = useState(post.likes || []);
+    const [open, setOpen] = useRecoilState(modalAtom);  
+    const [postId, setPostId] = useRecoilState(postIdAtom); 
+    const { user } = useUser(); 
+    const router  = useRouter(); 
 
-  const deletePost = async () => {
-    if (window.confirm('Are you sure you want to delete this post?')) {
-      if (user && user.publicMetadata.userMongoId === post.user) {
-        const res = await fetch('/api/post/delete', {
-          method: 'DELETE',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ postId: post._id }),
-        });
-        if (res.status === 200) {
-          location.reload();
-        } else {
-          alert('Error deleting post');
+    const likePost = () => {
+        if(!user) {
+            return router.push('/sign-in');  
         }
-      }
+        const like = fetch('/api/post/like', {
+            method: 'PUT', 
+            headers: {
+                'Content-Type' : 'application/json',
+            },
+            body: JSON.stringify({ postId: post._id}), 
+            // above here, we send the post._id information as postId; 
+        }); 
+        if(like && isLiked) {
+            setLikes(likes.filter((like) => (like) !== user.publicMetadata.userMongoId));
+            // above here, if it's already like, we wanna remove the like 
+        } 
+          if(like && !isLiked) {
+            setLikes([...likes, user.publicMetadata.userMongoId]); 
+            // above here, if it's not liked, like it, 
+        }
     }
-  };
+    useEffect(() => {
+        if(user && likes?.includes(user.publicMetadata.userMongoId)){
+            setIsLiked(true); 
+        } else {
+            setIsLiked(false);
+        }
+       
+    }, [likes, user]); 
 
-  return (
-    <div className='flex justify-start gap-5 p-2 text-gray-500'>
-      <div className='flex items-center'>
-        <HiOutlineChat
-          className='h-8 w-8 cursor-pointer rounded-full  transition duration-500 ease-in-out p-2 hover:text-sky-500 hover:bg-sky-100'
-          onClick={() => {
-            if (!user) {
-              router.push('/sign-in');
+    const deletePost = async () => {
+       if(window.confirm('Are you sure you want to delete this post?')){
+        if(user && user.publicMetadata.userMongoId === post.user) {
+            const res = await fetch('/api/post/delete', {
+                method: 'DELETE', 
+                headers: {
+                    'Content-Type': 'application/json', 
+                }, 
+                body: JSON.stringify({postId: post._id}), 
+            }); 
+            if (res.status === 200) {
+                location.reload(); 
             } else {
-              setOpen(!open);
-              setPostId(post._id);
+                alert('Error deleting post'); 
             }
-          }}
-        />
-        {post.comments.length > 0 && (
-          <span className='text-xs'>{post.comments.length}</span>
-        )}
-      </div>
-      <div className='flex items-center'>
-        {isLiked ? (
-          <HiHeart
-            onClick={likePost}
-            className='h-8 w-8 cursor-pointer rounded-full  transition duration-500 ease-in-out p-2 text-red-600 hover:text-red-500 hover:bg-red-100'
-          />
-        ) : (
-          <HiOutlineHeart
-            onClick={likePost}
-            className='h-8 w-8 cursor-pointer rounded-full  transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100'
-          />
-        )}
-        {likes.length > 0 && (
-          <span className={`text-xs ${isLiked && 'text-red-600'}`}>
-            {likes.length}
-          </span>
-        )}
-      </div>
-      {user && user.publicMetadata.userMongoId === post.user && (
-        <HiOutlineTrash
-          onClick={deletePost}
-          className='h-8 w-8 cursor-pointer rounded-full  transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100'
-        />
-      )}
-    </div>
-  );
+        }
+       }
+    }
+    return (
+        <div className="flex justify-start gap-5 p-2 text-gray-500">
+            <div className="flex items-center">
+            <HiOutlineChat 
+             className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-sky-500 hover:bg-sky-100" 
+             onClick={ () => {
+                if(!user) {
+                    router.push('/sign-in'); 
+                } else {
+                    setOpen(!open); 
+                    setPostId(post._id); 
+                }
+             }}
+             />
+             {post.comments.length > 0 && (
+                <span className="text-xs">{post.comments.length}</span> 
+             )}
+             </div> 
+            <div className="flex items-center">
+                {isLiked ? (
+                    <HiHeart 
+                    onClick={likePost}
+                    className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 text-red-600 hover:text-red-500 hover:bg-red-100 "
+                    />
+                ) : (
+                    <HiOutlineHeart 
+                    onClick={likePost}
+                    className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100"  />
+                )}
+                {likes.length > 0 && (
+                    <span className={`text-xs ${isLiked && 'text-red-600'}`}>
+                        {likes.length}
+                        </span>
+                    )}
+            </div>
+            {user && user.publicMetadata.userMongoId === post.user && <HiOutlineTrash onClick={deletePost} className="h-8 w-8 cursor-pointer rounded-full transtion duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100" />}
+             
+        </div>
+    ); 
 }
