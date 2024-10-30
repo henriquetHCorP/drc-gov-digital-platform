3f8db887c17df88d9c5fdc384dd93a15bcaecc28
henriquet kapema <henriquetkapema@gmail.com>
2024-10-30 01:17:15 +0100

    test4

--- src/app/api/post/like/route.js
+++ src/app/api/post/like/route.js
@@ -1,35 +1,33 @@
-import Post from '../../../../lib/models/post.model.js'; 
-import { connect } from '../../../../lib/mongodb/mongoose.js'; 
-import { currentUser } from '@clerk/nextjs/server'; 
+import Post from '../../../../lib/models/post.model';
+import { connect } from '../../../../lib/mongodb/mongoose';
+import { currentUser } from '@clerk/nextjs/server';
 
 export const PUT = async (req) => {
-  const user = await currentUser(); 
+  const user = await currentUser();
   try {
-   await connect(); 
-   const data = await req.json(); 
-   if(!user) {
-    return { status: 401, body: 'Unauthorized' }; 
-   }
-   const post = await Post.findById(data.postId); 
-   if(post.likes.includes(user.publicMetadata.userMongoId)) {
-  // above here,it means that you have already like the post and if you click you must remove the like
-  const updatedPost = await Post.findByIdAndUpdate(
-    data.postId, 
-    { $pull: { likes: user.publicMetadata.userMongoId } },
-     { new: true }
-    ); 
-  return new Response(JSON.stringify(updatedPost), {status: 200 }); 
-   } else {
-    // we wanna add the like
-    const updatedPost = await Post.findByIdAndUpdate(
-      data.postId, 
-      { $addToSet: { likes: user.publicMetadata.userMongoId } }, 
-      { new: true }
-    ); 
-    return new Response(JSON.stringify(updatedPost), { status: 200 }); 
-   }
-  } catch(error) {
-    console.log('Error liking the post:', error);
-    return new Response('Error liking post', {status: 500 });
+    await connect();
+    const data = await req.json();
+    if (!user) {
+      return { status: 401, body: 'Unauthorized' };
+    }
+    const post = await Post.findById(data.postId);
+    if (post.likes.includes(user.publicMetadata.userMongoId)) {
+      const updatedPost = await Post.findByIdAndUpdate(
+        data.postId,
+        { $pull: { likes: user.publicMetadata.userMongoId } },
+        { new: true }
+      );
+      return new Response(JSON.stringify(updatedPost), { status: 200 });
+    } else {
+      const updatedPost = await Post.findByIdAndUpdate(
+        data.postId,
+        { $addToSet: { likes: user.publicMetadata.userMongoId } },
+        { new: true }
+      );
+      return new Response(JSON.stringify(updatedPost), { status: 200 });
+    }
+  } catch (error) {
+    console.log('Error liking post:', error);
+    return new Response('Error liking post', { status: 500 });
   }
-}; 
+};
