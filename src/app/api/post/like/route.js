69e0f80dc5ca482a8b3febadf1dbe40c25aa896f
henriquet kapema <henriquetkapema@gmail.com>
2024-10-30 20:53:35 +0100

    amended2

--- src/app/api/post/like/route.js
+++ src/app/api/post/like/route.js
@@ -3,31 +3,31 @@ import { connect } from '../../../../lib/mongodb/mongoose';
 import { currentUser } from '@clerk/nextjs/server';
 
 export const PUT = async (req) => {
-//   const user = await currentUser();
+const user = await currentUser();
   try {
-    // await connect();
-    // const data = await req.json();
+    await connect();
+    const data = await req.json();
     if (!user) {
-      return { status: 401, body: 'Unauthorized' };
+    //   return { status: 401, body: 'Unauthorized' };
     }
-    // const post = await Post.findById(data.postId);
-    // if (post.likes.includes(user.publicMetadata.userMongoId)) {
-    //   const updatedPost = await Post.findByIdAndUpdate(
-    //     data.postId,
-    //     { $pull: { likes: user.publicMetadata.userMongoId } },
-    //     { new: true }
-    //   );
+    const post = await Post.findById(data.postId);
+    if (post.likes.includes(user.publicMetadata.userMongoId)) {
+      const updatedPost = await Post.findByIdAndUpdate(
+        data.postId,
+        { $pull: { likes: user.publicMetadata.userMongoId } },
+        { new: true }
+      );
     //   return new Response(JSON.stringify(updatedPost), { status: 200 });
-    // } else {
-    //   const updatedPost = await Post.findByIdAndUpdate(
-    //     data.postId,
-    //     { $addToSet: { likes: user.publicMetadata.userMongoId } },
-    //     { new: true }
-    //   );
+    } else {
+      const updatedPost = await Post.findByIdAndUpdate(
+        data.postId,
+        { $addToSet: { likes: user.publicMetadata.userMongoId } },
+        { new: true }
+      );
     //   return new Response(JSON.stringify(updatedPost), { status: 200 });
-    // }
+    }
   } catch (error) {
     console.log('Error liking post:', error);
-    return new Response('Error liking post', { status: 500 });
+    // return new Response('Error liking post', { status: 500 });
   }
 };
