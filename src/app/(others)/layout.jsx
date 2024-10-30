42a0e174cc50cfb03783926eef402e49f46b36d4
henriquet kapema <henriquetkapema@gmail.com>
2024-10-30 22:15:33 +0100

    with left sidebar

--- src/app/(others)/layout.jsx
+++ src/app/(others)/layout.jsx
@@ -38,7 +38,7 @@ export default function RootLayout({ children }) {
         </ClerkLoading>
         <ClerkLoaded>
         <div className="flex justify-between max-w-6xl mx-auto">
-          <div className="hidden sm:inline border-r h-screen sticky top-0">
+          <div className="sm:inline border-r h-screen sticky top-0">
             <LeftSidebar />
           </div>
             <div className="w-2xl flex-1">{children}</div>
